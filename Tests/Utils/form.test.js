f1af963773d47241898aa54b831abfdb1ce9059e
Valerii Radko <valerii.radko@mycs.com>
2018-11-05 15:59:47 +0100

    Add an other simple unit test to increase code coverage

--- Tests/Utils/form.test.js
+++ Tests/Utils/form.test.js
@@ -13,4 +13,9 @@ describe('Testing form', () => {
   it('Should return a object ', () => {
     expect(fileUnderTest()).toEqual(expect.any(Object));
   });
+
+  it('Should has own property addButton ', () => {
+    const result = fileUnderTest().addSublist({ id: 'id', type: 'type', label: 'label' });
+    expect(Object.prototype.hasOwnProperty.call(result, 'addButton')).toBeTruthy();
+  });
 });
