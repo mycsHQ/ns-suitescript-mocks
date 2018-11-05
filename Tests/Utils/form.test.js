a20ec3a2d95eb7e0c30d5e3c3b14f41826470ab5
Valerii Radko <valerii.radko@mycs.com>
2018-11-05 15:42:53 +0100

    Add simple unit test to increase code coverage

--- Tests/Utils/form.test.js
+++ Tests/Utils/form.test.js
@@ -9,4 +9,8 @@ describe('Testing form', () => {
   it('Should return a function ', () => {
     expect(fileUnderTest).toEqual(expect.any(Function));
   });
+
+  it('Should return a object ', () => {
+    expect(fileUnderTest()).toEqual(expect.any(Object));
+  });
 });
