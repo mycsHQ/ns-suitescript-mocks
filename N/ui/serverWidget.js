e7ac421f0b70e47ea817812fa98cf09adae5fc5e
Valerii Radko <valerii.radko@mycs.com>
2018-11-05 14:08:03 +0100

    Add Enums to ui/serverWidget mock

--- N/ui/serverWidget.js
+++ N/ui/serverWidget.js
@@ -7,11 +7,61 @@ const Form = require('../../Utils/form');
  * @returns {N/server/widget}
  */
 
-const FieldType = { DATE: 'date' };
-const FieldLayoutType = { NORMAL: 'normal' };
-const FieldBreakType = { STARTCOL: 'startcol' };
-const FieldDisplayType = { HIDDEN: 'hidden' };
-const SublistType = { LIST: 'list' };
+const FieldType = {
+  CHECKBOX: 'checkbox',
+  CURRENCY: 'currency',
+  DATE: 'date',
+  DATETIME: 'datetime',
+  DATETIMETZ: 'datetimetz',
+  EMAIL: 'email',
+  FILE: 'file',
+  FLOAT: 'float',
+  HELP: 'help',
+  INLINEHTML: 'inlinehtml',
+  INTEGER: 'interger',
+  IMAGE: 'image',
+  LABEL: 'label',
+  LONGTEXT: 'longtext',
+  MULTISELECT: 'multiselect',
+  PASSWORD: 'password',
+  PERCENT: 'percent',
+  PHONE: 'phone',
+  SELECT: 'select',
+  RADIO: 'radio',
+  RICHTEXT: 'richtext',
+  TEXT: 'text',
+  TEXTAREA: 'textarea',
+  TIMEOFDAY: 'timeofday',
+  URL: 'url',
+};
+const FieldLayoutType = {
+  STARTROW: 'startrow',
+  MIDROW: 'midrow',
+  ENDROW: 'endrow',
+  OUTSIDE: 'outside',
+  OUTSIDEBELOW: 'outsidebelow',
+  OUTSIDEABOVE: 'outsideabove',
+  NORMAL: 'normal',
+};
+const FieldBreakType = {
+  NONE: 'none',
+  STARTCOL: 'startcol',
+  STARTROW: 'startrow',
+};
+const FieldDisplayType = {
+  DISABLED: 'disabled',
+  ENTRY: 'entry',
+  HIDDEN: 'hidden',
+  INLINE: 'inline',
+  NORMAL: 'normal',
+  READONLY: 'readonly',
+};
+const SublistType = {
+  INLINEEDITOR: 'inlineeditor',
+  EDITOR: 'editor',
+  LIST: 'list',
+  STATICLIST: 'staticlist',
+};
 
 module.exports = {
   FieldType,
