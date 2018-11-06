e4feec5162a6f1f9c699f4fd87a23178e737be0c
Simon Camilleri <simon.camilleri@mycs.com>
2018-11-06 16:50:37 +0100

    unit tests for getFieldValues

--- Tests/Utils/record.test.js
+++ Tests/Utils/record.test.js
@@ -5,8 +5,21 @@
 
 const fileUnderTest = require('../../Utils/record');
 
-describe('Testing record', () => {
-  it('Should return a function ', () => {
+describe('testing record', () => {
+  it('should return a function ', () => {
     expect(fileUnderTest).toEqual(expect.any(Function));
   });
 });
+
+describe('setFieldValues', () => {
+  it('should check whether method exists', () => {
+    const result = new fileUnderTest('');
+    expect(result.hasOwnProperty('setFieldValues')).toBeTruthy();
+  });
+  it('should assign values to fieldName', () => {
+    const result = new fileUnderTest();
+    const values = ['123456', '654321']
+    result.setFieldValues('selectField', values);
+    expect(result.getFieldValue('selectField')).toEqual(values);
+  });
+})
