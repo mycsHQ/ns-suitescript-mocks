b4dcd94753e527dac90f36504f0255483f94d1f7
Valerii Radko <valerii.radko@mycs.com>
2019-10-31 12:16:51 +0100

    add unit test

--- Tests/N/url.test.js
+++ Tests/N/url.test.js
@@ -3,14 +3,23 @@
  *
  */
 
-const fileUnderTest = require('../../N/url');
+const record = require('../../N/record');
+const url = require('../../N/url');
 
 describe('Testing url module', () => {
   it('Should return object with properies available in Netsuite\'s url module ', () => {
     const expected = {
+      resolveDomain: expect.any(Function),
       resolveScript: expect.any(Function),
       resolveRecord: expect.any(Function),
     };
-    expect(fileUnderTest).toMatchObject(expected);
+
+    expect(url).toMatchObject(expected);
+  });
+
+  it('Should return transaction url', () => {
+    const result = url.resolveRecord({ recordType: record.Type.SALES_ORDER, recordId: 1234567 });
+
+    expect(result).toBe('/app/accounting/transactions/salesord.nl?id=1234567');
   });
 });
