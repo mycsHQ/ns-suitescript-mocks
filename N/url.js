190bc0b04b9e742755f6252e34aef51928e63ca1
Valerii Radko <valerii.radko@mycs.com>
2019-10-31 11:15:40 +0100

    add resolveDomain method to url module

--- N/url.js
+++ N/url.js
@@ -7,6 +7,14 @@
  */
 
 module.exports = {
+  HostType: {
+    APPLICATION: 'APPLICATION',
+    CUSTOMER_CENTER: 'CUSTOMER_CENTER',
+    FORM: 'FORM',
+    RESTLET: 'RESTLET',
+    SUITETALK: 'SUITETALK',
+  },
+  resolveDomain: () => 'system.eu2.netsuite.com',
+  resolveRecord: () => 'https://system.eu2.netsuite.com/app/accounting/transactions/salesord.nl',
   resolveScript: obj => `https://system.eu2.netsuite.com/app/site/hosting/scriptlet.nl?script=${obj.scriptId}&deploy=${obj.deploymentId}`,
-  resolveRecord: () => 'https://system.eu2.netsuite.com//app/accounting/transactions/salesord.nl',
 };
