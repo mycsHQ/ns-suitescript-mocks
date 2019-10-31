1004a3abf28f343b7738a55603846f4c498dbbd0
Valerii Radko <valerii.radko@mycs.com>
2019-10-31 15:22:03 +0100

    N/url enums (#26)
    * add resolveDomain method to url module
    
    * update version
    
    * update resolveRecord method on url module
    
    * add unit test

--- N/url.js
+++ N/url.js
@@ -6,7 +6,22 @@
  * @returns {N/url}
  */
 
+const record = require('./record');
+
+const transactionPath = Object.freeze({
+  [record.Type.PURCHASE_ORDER]: 'purchord',
+  [record.Type.SALES_ORDER]: 'salesord',
+});
+
 module.exports = {
-  resolveScript: obj => `https://system.eu2.netsuite.com/app/site/hosting/scriptlet.nl?script=${obj.scriptId}&deploy=${obj.deploymentId}`,
-  resolveRecord: () => 'https://system.eu2.netsuite.com//app/accounting/transactions/salesord.nl',
+  HostType: {
+    APPLICATION: 'APPLICATION',
+    CUSTOMER_CENTER: 'CUSTOMER_CENTER',
+    FORM: 'FORM',
+    RESTLET: 'RESTLET',
+    SUITETALK: 'SUITETALK',
+  },
+  resolveDomain: () => 'system.eu2.netsuite.com',
+  resolveRecord: ({ recordType, recordId }) => `/app/accounting/transactions/${transactionPath[recordType]}.nl?id=${recordId}`,
+  resolveScript: ({ scriptId, deploymentId }) => `https://system.eu2.netsuite.com/app/site/hosting/scriptlet.nl?script=${scriptId}&deploy=${deploymentId}`,
 };
